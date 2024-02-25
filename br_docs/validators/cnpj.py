0c7c4f7e2c8849fea7f751aa59e3de34eb261ee0
Vinícius Aguiar <vaguiararqdevsoftware@proton.me>
2024-02-25 13:24:25 -0300

    refactor(CNPJv): class attribute renamed
    Took 14 seconds

--- br_docs/validators/cnpj.py
+++ br_docs/validators/cnpj.py
@@ -5,7 +5,7 @@ from br_docs.validators import CheckTwoDigits
 
 class CNPJv(CheckTwoDigits):
     Patterns = re.compile(r"^\d{14}$"), re.compile(r"^[0-9]{2}\.[0-9]{3}\.[0-9]{3}/[0-9]{4}-[0-9]{2}$"),
-    CnpjAlgarisms = 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2,
+    CnpjAlgarismsMultipliers = 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2,
 
     @classmethod
     def calculate_digits(cls, non_digits: list[int]) -> tuple[int, int]:
@@ -13,13 +13,13 @@ class CNPJv(CheckTwoDigits):
             CNPJ's digits checking algorithm.
             https://web.archive.org/web/20240222180515/https://www.macoratti.net/alg_cnpj.htm
         """
-        calc = sum(x*y for x, y in zip(non_digits, cls.CnpjAlgarisms[1:])) % 11
+        calc = sum(x*y for x, y in zip(non_digits, cls.CnpjAlgarismsMultipliers[1:])) % 11
         if calc < 2:
             digit_one = 0
         else:
             digit_one = 11 - calc
         non_digits.append(digit_one)
-        calc_2 = sum(x*y for x, y in zip(non_digits, cls.CnpjAlgarisms)) % 11
+        calc_2 = sum(x*y for x, y in zip(non_digits, cls.CnpjAlgarismsMultipliers)) % 11
         if calc_2 < 2:
             digit_two = 0
         else:
