2bb4efd5aad5e667d1d534a59da3a5be4094131f
Rafael Kamimura <rafael.kamimura@gmail.com>
2024-05-13 15:05:25 -0300

    chore: formatting back to defaults since vim decided to use double quotes

--- tests/test_brdocs.py
+++ tests/test_brdocs.py
@@ -14,57 +14,57 @@ def validate(model_name: str, values, value_type):
                 m = create_model(model_name, param=(value_type, ...))
                 m(param=value)
             except ValidationError as exc:
-                assert False, f"{exc}: {value}"
+                assert False, f'{exc}: {value}'
         yield
     finally:
         pass
 
 
 def test_cnh(cnh_list):
-    with validate(model_name="TestCNH", values=cnh_list, value_type=CNH):
+    with validate(model_name='TestCNH', values=cnh_list, value_type=CNH):
         pass
 
 
 def test_cpf(cpf_list):
-    with validate(model_name="TestCPF", values=cpf_list, value_type=CPF):
+    with validate(model_name='TestCPF', values=cpf_list, value_type=CPF):
         pass
 
 
 def test_cnpj(cnpj_list):
-    with validate(model_name="TestCNPJ", values=cnpj_list, value_type=CNPJ):
+    with validate(model_name='TestCNPJ', values=cnpj_list, value_type=CNPJ):
         pass
 
 
 def test_nis(nis_list):
-    with validate(model_name="TestNIS", values=nis_list, value_type=NIS):
+    with validate(model_name='TestNIS', values=nis_list, value_type=NIS):
         pass
 
 
 def test_cns(cns_list):
-    with validate(model_name="TestCNS", values=cns_list, value_type=CNS):
+    with validate(model_name='TestCNS', values=cns_list, value_type=CNS):
         pass
 
 
 def test_renavam(renavam_list):
-    with validate(model_name="TestRENAVAM", values=renavam_list, value_type=RENAVAM):
+    with validate(model_name='TestRENAVAM', values=renavam_list, value_type=RENAVAM):
         pass
 
 
 def test_te(te_list):
-    with validate(model_name="TestTE", values=te_list, value_type=TE):
+    with validate(model_name='TestTE', values=te_list, value_type=TE):
         pass
 
 
 def test_cert(cert_list):
-    with validate(model_name="TestCERT", values=cert_list, value_type=CERT):
+    with validate(model_name='TestCERT', values=cert_list, value_type=CERT):
         pass
 
 
 def test_valid_sei(valid_sei_list):
-    with validate(model_name="TestSEI", values=valid_sei_list, value_type=SEI):
+    with validate(model_name='TestSEI', values=valid_sei_list, value_type=SEI):
         pass
 
 
 def test_invalid_sei(invalid_sei_list):
-    with validate(model_name="TestSEI", values=invalid_sei_list, value_type=SEI):
+    with validate(model_name='TestSEI', values=invalid_sei_list, value_type=SEI):
         pass
