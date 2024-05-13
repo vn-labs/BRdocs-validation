5a905f2c7739a8f93a3b9f7ac6658c2a30523997
Rafael Kamimura <rafael.kamimura@gmail.com>
2024-05-13 14:48:43 -0300

    feat: add SEI validator

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,73 +7,83 @@ current_path = Path(__file__).parent
 
 def open_valid_docs(path: str) -> Iterator[str]:
     with open(str(current_path.joinpath(path))) as f:
-        return map(lambda x: x.strip('\n'), f.readlines())
+        return map(lambda x: x.strip("\n"), f.readlines())
 
 
 @pytest.fixture
 def cnh_list():
     """
-        1-69 line: https://web.archive.org/web/20240223023315/https://dodf.df.gov.br/index/visualizar-arquivo/?pasta=2023|01_Janeiro|DODF%20022%2031-01-2023|&arquivo=DODF%20022%2031-01-2023%20INTEGRA.pdf
-        70-100 line: https://web.archive.org/web/20240224034619/https://dodf.df.gov.br/index/visualizar-arquivo/?pasta=2021%7C11_Novembro%7CDODF%20208%2008-11-2021%7C&arquivo=DODF%20208%2008-11-2021%20INTEGRA.pdf
-        101-125 line: https://web.archive.org/web/20240224035353/https://dodf.df.gov.br/index/visualizar-arquivo/?pasta=2021%7C12_Dezembro%7CDODF%20232%2014-12-2021%7C&arquivo=DODF%20232%2014-12-2021%20INTEGRA.pdf
+    1-69 line: https://web.archive.org/web/20240223023315/https://dodf.df.gov.br/index/visualizar-arquivo/?pasta=2023|01_Janeiro|DODF%20022%2031-01-2023|&arquivo=DODF%20022%2031-01-2023%20INTEGRA.pdf
+    70-100 line: https://web.archive.org/web/20240224034619/https://dodf.df.gov.br/index/visualizar-arquivo/?pasta=2021%7C11_Novembro%7CDODF%20208%2008-11-2021%7C&arquivo=DODF%20208%2008-11-2021%20INTEGRA.pdf
+    101-125 line: https://web.archive.org/web/20240224035353/https://dodf.df.gov.br/index/visualizar-arquivo/?pasta=2021%7C12_Dezembro%7CDODF%20232%2014-12-2021%7C&arquivo=DODF%20232%2014-12-2021%20INTEGRA.pdf
     """
-    return open_valid_docs('cnhs.txt')
+    return open_valid_docs("cnhs.txt")
 
 
 @pytest.fixture
 def cpf_list():
     """
-        1-60 line: https://web.archive.org/web/20240223042316/https://www.detran.ac.gov.br/wp-content/uploads/2022/06/Lista-de-Selecionados-CNH-Social.pdf
-        61-122 line: https://web.archive.org/web/20240226031446/https://www.detran.ac.gov.br/wp-content/uploads/2022/06/Lista-de-Selecionados-CNH-Social.pdf
+    1-60 line: https://web.archive.org/web/20240223042316/https://www.detran.ac.gov.br/wp-content/uploads/2022/06/Lista-de-Selecionados-CNH-Social.pdf
+    61-122 line: https://web.archive.org/web/20240226031446/https://www.detran.ac.gov.br/wp-content/uploads/2022/06/Lista-de-Selecionados-CNH-Social.pdf
     """
-    return open_valid_docs('cpfs.txt')
+    return open_valid_docs("cpfs.txt")
 
 
 @pytest.fixture
 def cnpj_list():
     """
-        1-88 line: https://web.archive.org/web/20240223150643/https://www.detran.df.gov.br/wp-content/uploads/2021/09/Empresas-CNH-Social.pdf
+    1-88 line: https://web.archive.org/web/20240223150643/https://www.detran.df.gov.br/wp-content/uploads/2021/09/Empresas-CNH-Social.pdf
     """
-    return open_valid_docs('cnpj.txt')
+    return open_valid_docs("cnpj.txt")
 
 
 @pytest.fixture
 def nis_list():
     """
-        1-56 line: https://web.archive.org/web/20240225200956/http://www.varzeagrande.mt.gov.br/storage/Anexos/18cd1d8751995dccb6116b97ab9e0ce7.pdf
-        57-356 line: http://www.adcon.rn.gov.br/ACERVO/SEARH/DOC/DOC000000000151962.PDF
+    1-56 line: https://web.archive.org/web/20240225200956/http://www.varzeagrande.mt.gov.br/storage/Anexos/18cd1d8751995dccb6116b97ab9e0ce7.pdf
+    57-356 line: http://www.adcon.rn.gov.br/ACERVO/SEARH/DOC/DOC000000000151962.PDF
     """
-    return open_valid_docs('nis.txt')
+    return open_valid_docs("nis.txt")
 
 
 @pytest.fixture
 def cns_list():
     """
-        1-61 line: https://web.archive.org/web/20240226014647/https://simaodias.se.gov.br/sites/simaodias.se.gov.br/files/LISTA%20VACINADOS%20D2%20IDOSOS%20-%2010032021%20-%20FORMULARIO.pdf
-        62-182 line: https://web.archive.org/web/20240226015830/https://altamira.pa.gov.br/wp-content/uploads/2021/07/LISTA-DE-VACINADOS-20-07.pdf
+    1-61 line: https://web.archive.org/web/20240226014647/https://simaodias.se.gov.br/sites/simaodias.se.gov.br/files/LISTA%20VACINADOS%20D2%20IDOSOS%20-%2010032021%20-%20FORMULARIO.pdf
+    62-182 line: https://web.archive.org/web/20240226015830/https://altamira.pa.gov.br/wp-content/uploads/2021/07/LISTA-DE-VACINADOS-20-07.pdf
     """
-    return open_valid_docs('cns.txt')
+    return open_valid_docs("cns.txt")
 
 
 @pytest.fixture
 def renavam_list():
     """
-        1-173 line: https://web.archive.org/web/20240226050829/https://www.euamoleilao.com.br/imprimir/0067-leilao-do-detran-de-sao-paulo
-        174-257 line: https://web.archive.org/web/20240226051016/https://www.euamoleilao.com.br/imprimir/0139-repasse-leilao-ciretran-s-sebastiao
-        258-687 line: https://web.archive.org/web/20240226051542/https://portal.tjpr.jus.br/pesquisa_athos/publico/carregarAnexo.do;jsessionid=5ff0c586fab5cb74176b49c48765?tjpr.url.crypto=16c74de0ca500657bb7c1cc39118d26e6a12b4f4c9aa9444c033d87933160fa249878bb1b73255ac
+    1-173 line: https://web.archive.org/web/20240226050829/https://www.euamoleilao.com.br/imprimir/0067-leilao-do-detran-de-sao-paulo
+    174-257 line: https://web.archive.org/web/20240226051016/https://www.euamoleilao.com.br/imprimir/0139-repasse-leilao-ciretran-s-sebastiao
+    258-687 line: https://web.archive.org/web/20240226051542/https://portal.tjpr.jus.br/pesquisa_athos/publico/carregarAnexo.do;jsessionid=5ff0c586fab5cb74176b49c48765?tjpr.url.crypto=16c74de0ca500657bb7c1cc39118d26e6a12b4f4c9aa9444c033d87933160fa249878bb1b73255ac
     """
-    return open_valid_docs('renavam.txt')
+    return open_valid_docs("renavam.txt")
 
 
 @pytest.fixture
 def te_list():
     """
-        1-159 line: https://web.archive.org/web/20240226104403/https://issuu.com/psol.df/docs/lista_de_filiados_ao_psol_df___dist
+    1-159 line: https://web.archive.org/web/20240226104403/https://issuu.com/psol.df/docs/lista_de_filiados_ao_psol_df___dist
     """
-    return open_valid_docs('te.txt')
+    return open_valid_docs("te.txt")
 
 
 @pytest.fixture
 def cert_list():
-    """ google dorks: intext:"DATA DE NASCIMENTO (POR EXTENSO)" AND intext:"CERTIDÃO DE <CASAMENTO|ÓBITO|NASCIMENTO>" AND intext:"matricula" AND ext:pdf """
-    return open_valid_docs('cert.txt')
+    """google dorks: intext:"DATA DE NASCIMENTO (POR EXTENSO)" AND intext:"CERTIDÃO DE <CASAMENTO|ÓBITO|NASCIMENTO>" AND intext:"matricula" AND ext:pdf"""
+    return open_valid_docs("cert.txt")
+
+
+@pytest.fixture
+def valid_sei_list():
+    return open_valid_docs("valid_sei.txt")
+
+
+@pytest.fixture
+def invalid_sei_list():
+    return open_valid_docs("invalid_sei.txt")
--- tests/test_brdocs.py
+++ tests/test_brdocs.py
@@ -3,7 +3,7 @@ from contextlib import contextmanager
 from pydantic import create_model
 from pydantic_core import ValidationError
 
-from br_docs import CNH, CPF, CNPJ, NIS, CNS, RENAVAM, TE, CERT
+from br_docs import CNH, CPF, CNPJ, NIS, CNS, RENAVAM, TE, CERT, SEI
 
 
 @contextmanager
@@ -21,40 +21,50 @@ def validate(model_name: str, values, value_type):
 
 
 def test_cnh(cnh_list):
-    with validate(model_name='TestCNH', values=cnh_list, value_type=CNH):
+    with validate(model_name="TestCNH", values=cnh_list, value_type=CNH):
         pass
 
 
 def test_cpf(cpf_list):
-    with validate(model_name='TestCPF', values=cpf_list, value_type=CPF):
+    with validate(model_name="TestCPF", values=cpf_list, value_type=CPF):
         pass
 
 
 def test_cnpj(cnpj_list):
-    with validate(model_name='TestCNPJ', values=cnpj_list, value_type=CNPJ):
+    with validate(model_name="TestCNPJ", values=cnpj_list, value_type=CNPJ):
         pass
 
 
 def test_nis(nis_list):
-    with validate(model_name='TestNIS', values=nis_list, value_type=NIS):
+    with validate(model_name="TestNIS", values=nis_list, value_type=NIS):
         pass
 
 
 def test_cns(cns_list):
-    with validate(model_name='TestCNS', values=cns_list, value_type=CNS):
+    with validate(model_name="TestCNS", values=cns_list, value_type=CNS):
         pass
 
 
 def test_renavam(renavam_list):
-    with validate(model_name='TestRENAVAM', values=renavam_list, value_type=RENAVAM):
+    with validate(model_name="TestRENAVAM", values=renavam_list, value_type=RENAVAM):
         pass
 
 
 def test_te(te_list):
-    with validate(model_name='TestTE', values=te_list, value_type=TE):
+    with validate(model_name="TestTE", values=te_list, value_type=TE):
         pass
 
 
 def test_cert(cert_list):
-    with validate(model_name='TestCERT', values=cert_list, value_type=CERT):
+    with validate(model_name="TestCERT", values=cert_list, value_type=CERT):
+        pass
+
+
+def test_valid_sei(valid_sei_list):
+    with validate(model_name="TestSEI", values=valid_sei_list, value_type=SEI):
+        pass
+
+
+def test_invalid_sei(invalid_sei_list):
+    with validate(model_name="TestSEI", values=invalid_sei_list, value_type=SEI):
         pass
