411190c25cb16f39a4c21bf5c786b9bfd23dbd6d
Rafael Kamimura <rafael.kamimura@gmail.com>
2024-05-13 15:21:43 -0300

    fix: rm print statement

--- br_docs/validators/sei.py
+++ br_docs/validators/sei.py
@@ -4,7 +4,7 @@ from pydantic_core import PydanticCustomError
 
 
 class SEIv:
-    Patterns = re.compile(r"^\d{5}-?\d{8}/?\d{4}-?\d{2}$")
+    Patterns = re.compile(r'^\d{5}-?\d{8}/?\d{4}-?\d{2}$')
 
     def __call__(self, value: str) -> str:
         self.validate(value)
@@ -12,7 +12,6 @@ class SEIv:
 
     def validate(self, value: str) -> str:
         check_format = bool(self.Patterns.match(value))
-        print(check_format)
         if check_format is not True:
-            raise PydanticCustomError("invalid", "Invalid Value")
+            raise PydanticCustomError('invalid', 'Invalid Value')
         return value
